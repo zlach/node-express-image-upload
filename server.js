4454c605ef710f9b45a59799a456b50488c8a5f5
Zachary Goldberg <zacharysp@gmail.com>
2019-06-19 18:17:38 -0500

    final

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require('./router');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.use('/upload', router);
+
+app.listen(port, function () {
+  console.log('Server is running on PORT', port);
+});
